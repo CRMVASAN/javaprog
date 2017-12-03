a350c162555825291c2cb0952555828bab7043f5
CRMVASAN <33810028+CRMVASAN@users.noreply.github.com>
2017-12-03 06:47:41 +0530

    added java progs

---
+++ name.java
@@ -0,0 +1,12 @@
+class name {
+public static void main(String args[])
+{
+int student_no= 100;  // variable
+System.out.println("Hello Kishore");
+System.out.println("Hello Kishore");
+System.out.println("stdudent number is"+student_no);
+} 
+
+
+
+}
---
+++ test.java
@@ -0,0 +1,8 @@
+class test {
+
+
+public static void main(String s args[])
+{
+Sytem.out.println("Hello World"); 
+}
+}
